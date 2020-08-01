d7095719ff870cef2e900d0162b2815e17687f05
Nithin Antony <nithinparathara007@gmail.com>
2020-08-01 10:20:01 +0530

    fix build error

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,82 +1,69 @@
 const path = require("path");
-const webpack = require("webpack");
 const Htmlwebpackplugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const dotenv = require("dotenv");
 
-module.exports = () => {
-  // call dotenv and it will return an Object with a parsed key
-  const env = dotenv.config().parsed;
-  // reduce it to a nice object, the same as before
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
-  return {
-    entry: path.join(__dirname, "./src/index.js"),
-    output: {
-      filename: "bundle.[contenthash].js",
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/",
-    },
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              inline: 1, // this right here
-              // keep_fnames: true
-            },
-            mangle: {
-              // keep_fnames: true
-            },
+module.exports = {
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            inline: 1, // this right here
+            // keep_fnames: true
           },
-        }),
-      ],
-    },
-    mode: "production",
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          use: ["babel-loader"],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          loader: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-        {
-          test: /\.scss$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.(jpg|png)$/,
-          use: {
-            loader: "url-loader",
+          mangle: {
+            // keep_fnames: true
           },
         },
-      ],
-    },
-    plugins: [
-      new TerserPlugin(),
-      new Htmlwebpackplugin({
-        template: path.join(__dirname, "/src/index.html"),
-        filename: "index.html",
-        inject: "body",
-        //favicon: "./src/images/favicon-32x32.png"
       }),
-      new MiniCssExtractPlugin({
-        filename: "[name].[contenthash].css",
-      }),
-      new CleanWebpackPlugin({
-        cleanAfterEveryBuildPatterns: ["dist"],
-      }),
-      new webpack.DefinePlugin(envKeys),
     ],
-  };
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new Htmlwebpackplugin({
+      template: path.join(__dirname, "/src/index.html"),
+      filename: "index.html",
+      inject: "body",
+      //favicon: "./src/images/favicon-32x32.png"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
+  ],
 };
